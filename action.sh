2ba6171a9ebe82f18d3a65b212125502ac5717f1
Prashant Warrier <prashant.warrier@privatecircle.co>
2021-12-23 10:45:53 +0530

    change curl invocation for `sops` download

--- action.sh
+++ action.sh
@@ -21,6 +21,6 @@ helm plugin install https://github.com/jkroepke/helm-secrets --version v3.11.0
 # Install sops
 SOPS_VERSION="v3.7.1"
 SOPS_OS="linux"
-curl -LO sops "https://github.com/mozilla/sops/releases/download/$SOPS_VERSION/$SOPS_OS"
+curl -L "https://github.com/mozilla/sops/releases/download/$SOPS_VERSION/$SOPS_OS" -O sops
 chmod +x sops
 sudo install -o root -g root -m 0755 sops /usr/local/bin/sops
