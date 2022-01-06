b943e40931382ff340485a4283aa58dbe12aa1c0
prashant-warrier-echelonvi <84698610+prashant-warrier-echelonvi@users.noreply.github.com>
2022-01-06 11:29:50 +0530

    pinning helm diff version, workaround for https://github.com/databus23/helm-diff/issues/322 (#4)
    pinning helm secrets version while we are at it
    
    Co-authored-by: Prashant Warrier <warrierprashant93@outlook.com>

--- action.sh
+++ action.sh
@@ -14,10 +14,13 @@ curl -LO https://github.com/roboll/helmfile/releases/download/v0.140.0/helmfile_
 sudo install -o root -g root -m 0755 helmfile_linux_amd64 /usr/local/bin/helmfile
 
 # Setup helm plugin `helm-diff`
-helm plugin install https://github.com/databus23/helm-diff
+HELM_DIFF_VERSION="3.1.3"
+helm plugin install https://github.com/databus23/helm-diff --version "$HELM_DIFF_VERSION"
 
 # Setup helm plugin `helm-secrets`
-helm plugin install https://github.com/jkroepke/helm-secrets --version v3.11.0
+HELM_SECERTS_VERSION="3.11.0"
+helm plugin install https://github.com/jkroepke/helm-secrets --version v"$HELM_SECERTS_VERSION"
+
 # Install sops
 SOPS_VERSION="v3.7.1"
 SOPS_OS="linux"
