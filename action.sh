563c7c21942f6eee62006c6953e221fccfbbb7f1
Prashant Warrier <prashant.warrier@privatecircle.co>
2023-01-12 14:56:52 +0530

    set helm secrets version to 4.1.1

--- action.sh
+++ action.sh
@@ -18,7 +18,7 @@ HELM_DIFF_VERSION="3.1.3"
 helm plugin install https://github.com/databus23/helm-diff --version "$HELM_DIFF_VERSION"
 
 # Setup helm plugin `helm-secrets`
-HELM_SECERTS_VERSION="3.11.0"
+HELM_SECERTS_VERSION="4.1.1"
 helm plugin install https://github.com/jkroepke/helm-secrets --version v"$HELM_SECERTS_VERSION"
 
 # Install sops
