bde3439dd3945c3b339d4aff7bf075e86f237410
prashant-warrier-echelonvi <84698610+prashant-warrier-echelonvi@users.noreply.github.com>
2023-02-01 15:42:09 +0530

    lose the beginning v from the $HELMFILE_VERSION variable, use updated asset name within download URL (#8)

--- action.sh
+++ action.sh
@@ -11,8 +11,8 @@ echo "$KUBE_CONFIG_DATA" | base64 --decode > ~/.kube/config
 sudo chmod 600 ~/.kube/config
 
 
-HELMFILE_VERSION="v0.148.1"
-curl -Lo helmfile.tar.gz https://github.com/helmfile/helmfile/releases/download/${HELMFILE_VERSION}/helmfile_linux_amd64.tar.gz
+HELMFILE_VERSION="0.148.1"
+curl -Lo helmfile.tar.gz https://github.com/helmfile/helmfile/releases/download/v${HELMFILE_VERSION}/helmfile_${HELMFILE_VERSION}_linux_amd64.tar.gz
 sudo tar -xf helmfile.tar.gz -C /usr/local/bin/
 sudo chmod 0755 /usr/local/bin/helmfile
 
