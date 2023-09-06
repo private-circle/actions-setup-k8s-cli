348ad4a1cdad68ceb689f1088e9c5db5a352f104
prashant-warrier-echelonvi <84698610+prashant-warrier-echelonvi@users.noreply.github.com>
2023-09-06 10:59:44 +0530

    Download and install latest helm

--- action.sh
+++ action.sh
@@ -11,10 +11,14 @@ echo "$KUBE_CONFIG_DATA" | base64 --decode > ~/.kube/config
 sudo chmod 600 ~/.kube/config
 
 
-HELMFILE_VERSION="0.148.1"
+HELMFILE_VERSION="0.156.0"
 curl -Lo helmfile.tar.gz https://github.com/helmfile/helmfile/releases/download/v${HELMFILE_VERSION}/helmfile_${HELMFILE_VERSION}_linux_amd64.tar.gz
 sudo tar -xf helmfile.tar.gz -C /usr/local/bin/
 sudo chmod 0755 /usr/local/bin/helmfile
+
+HELM_VERSION="v3.12.3"
+curl -Lo helm.tar.gz https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz
+sudo tar -xf helm.tar.gz linux-amd64/helm --strip-components 1 -C /usr/local/bin/helm
 
 # Setup helm plugin `helm-diff`
 HELM_DIFF_VERSION="3.6.0"
