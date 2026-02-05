9e274e54f7af8dff1f65f5582ab67eeffdc63bee
sedhupathygit <sedhupathy@privatecircle.co>
2026-02-05 16:18:13 +0530

    Idompotent plugin install fix

--- action.sh
+++ action.sh
@@ -31,13 +31,13 @@ curl -Lo helmfile.tar.gz https://github.com/helmfile/helmfile/releases/download/
 sudo tar -xf helmfile.tar.gz -C /usr/local/bin/
 sudo chmod 0755 /usr/local/bin/helmfile
 
-# Setup helm plugin `helm-diff`
+# Setup helm plugin `helm-diff` (skip if already installed)
 HELM_DIFF_VERSION="3.6.0"
-helm plugin install https://github.com/databus23/helm-diff --version "$HELM_DIFF_VERSION"
+helm plugin list 2>/dev/null | grep -q diff || helm plugin install https://github.com/databus23/helm-diff --version "$HELM_DIFF_VERSION"
 
-# Setup helm plugin `helm-secrets`
+# Setup helm plugin `helm-secrets` (skip if already installed)
 HELM_SECRETS_VERSION="4.6.5"
-helm plugin install https://github.com/jkroepke/helm-secrets --version v"$HELM_SECRETS_VERSION"
+helm plugin list 2>/dev/null | grep -q secrets || helm plugin install https://github.com/jkroepke/helm-secrets --version v"$HELM_SECRETS_VERSION"
 
 # Install sops
 SOPS_VERSION="v3.7.1"
